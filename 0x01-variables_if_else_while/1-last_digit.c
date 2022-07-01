8756c17c9b0abeb76b2bc877779bf66357233b3b
get1221 <get1221smart@gmail.com>
2022-07-01 13:38:54 -0700

    lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,6 +17,6 @@ printf("Last digit of %d is %d and is 0\n", n, n % 10);
 else if ((n % 10) > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 else
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+printf("last digit of %d is %d and is lessthan 6 and not 0\n", n, n % 10);
 return (0);
 }
