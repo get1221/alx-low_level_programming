e10357cede1ce68450f94056afba4b77e3c7b26b
get1221 <get1221smart@gmail.com>
2022-07-02 10:04:04 -0700

    print small and capital letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - main code
- * Descrption : printout small letters and capital letters
+ * Description : printout small letters and capital letters
  * Return : 0
  */
 int main(void)
