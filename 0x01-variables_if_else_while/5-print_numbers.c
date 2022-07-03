e16adb96abcca10864f8ca18890596211dcbd5d6
get1221 <get1221smart@gmail.com>
2022-07-03 03:01:08 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 /**
  *  main - main block 
+ *
  *  Descriptions: print base10 numbers
  *  Return: 0
  */
 int main(void)
 {
 int num;
-	
-	for (num = 0; num < 10; num++)
-		printf("%i", num);
-	putchar('\n');
-	return (0);
+
+for (num = 0; num < 10; num++)
+printf("%i", num);
+putchar('\n');
+return (0);
 }
