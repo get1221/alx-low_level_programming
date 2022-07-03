2000be0acfbcb0c626b987730f137ee0a65bb719
get1221 <get1221smart@gmail.com>
2022-07-03 05:10:01 -0700

    alphabet and digit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 	int num;
 	char get;
 
-	for (num =0; num < 10; num++)
-	for (get ='a'; get < 'g'; get++)
+	for (num = 0; num < 10; num++)
+	for (get = 'a'; get < 'g'; get++)
 		putchar(num);
 	putchar(get);
-	       putchar('\n');
-	       return (0);
+       	putchar('\n');
+       	return (0);
 }	
