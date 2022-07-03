b0dfd1750dead7d19204b87b7b61a60b844c643f
get1221 <get1221smart@gmail.com>
2022-07-03 04:55:14 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print lowercase and numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char get;
+
+	for (num =0; num < 10; num++)
+	for (get ='a'; get < g; get++)
+		putchar(num,get);
+	       putchar('\n')
+	       return (0);
+}	
