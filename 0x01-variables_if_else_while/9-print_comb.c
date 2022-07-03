f55536d35a4f09339903cba89bb0c76da573d36c
get1221 <get1221smart@gmail.com>
2022-07-03 06:04:59 -0700

    num space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *
+ * main - print numbers
+ * Returns: 0
+ */
+int main(void)
+{
+	num = 0;
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar(',')
+		putchar( );
+	putchar('\n');
+	return (0);
+}
