2970b8cd50e1b520fa4bec1b04565559abb80a75
get1221 <get1221smart@gmail.com>
2022-07-03 02:30:55 -0700

    display alphabet excep e andq

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*
+ * main - main block code
+ * Descriptions: print alphabet except e and q
+ * Return: 0 (suucess)
+ */
+int main(void)
+{
+	char let;
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		if (let != 'e' && let != 'q')
+			putchar(let);
+	}
+	putchar('\n');
+
+	return (0);
+}
