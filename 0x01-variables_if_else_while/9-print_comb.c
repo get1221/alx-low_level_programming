cb6f4dc306e02181ef61450ecb5b465c5ba7ba94
get1221 <get1221smart@gmail.com>
2022-07-03 06:36:34 -0700

    numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
  * main - print numbers by comma and space
- * Description: Print Numbers separated by ,, followed by a space in  ascending order
+ * Description: Print Numbers separated by 
+ * ,, followed by a space in  ascending order
  * Return: 0
  */
 int main(void)
