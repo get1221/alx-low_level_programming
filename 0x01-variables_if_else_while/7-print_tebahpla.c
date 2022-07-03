0f5fde3b8fd572f3b630416c66efdb37c8f7d8cb
get1221 <get1221smart@gmail.com>
2022-07-03 04:38:04 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print in reverse alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char = let;
+
+	for (let = 'z' let >= 'a'; let--)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
