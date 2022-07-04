5edaccfa164ff0e3b9f33e3343599b119cb6abc4
get1221 <get1221smart@gmail.com>
2022-07-04 03:06:39 -0700

    number combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdiolib.>
+/**
+ * main - combination of two two-digit numbers
+ * Return 0
+ */
+int main(void)
+{
+	int g, h;
+
+	for (g = 0; g <= 99; g++)
+	{
+		for (h = 1; h <= 99; h++)
+		{
+			if (h != g)
+			{
+				putchar(g / 10 + 48);
+				putchar(g % 10 + 48);
+				putchar(' ');
+				putchar(h / 10 + 48);
+				putchar(h % 10 +48);
+				if (g * 100 +h !=9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
