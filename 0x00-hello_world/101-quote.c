885a5d6cb4206baa1b268c4dd057e368525e5cd7
get1221 <get1221smart@gmail.com>
2022-07-04 03:50:36 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include<unlistd.>
 /**
  * main - standand output
  * Return: Always 0
  */
 int main(void)
 {
-	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
