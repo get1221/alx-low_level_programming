b3e7bb0448be64ee94e718783d131e4f5c891f09
get1221 <get1221smart@gmail.com>
2022-07-05 12:49:40 -0700

    hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int g, h;
+	g = 0;
+	while (g < 24)
+	{
+		h = 0;
+		while (h < 60)
+		{
+			_putchar((g / 10) +'0');
+			_putchar((g % 10) + '0');
+			_putchar(':');
+			_putchar((h / 10) + '0');
+			_putchar(h / 10) + '0');
+			_putchar('\n');
+			h++;
+		}
+		g++;
+	}
+}
