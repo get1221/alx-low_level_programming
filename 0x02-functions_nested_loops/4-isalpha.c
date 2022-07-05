6a15b85489dcad9b8f516292bf0a1669461ce968
get1221 <get1221smart@gmail.com>
2022-07-05 06:08:33 -0700

    alphabet check

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,14 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
 	
