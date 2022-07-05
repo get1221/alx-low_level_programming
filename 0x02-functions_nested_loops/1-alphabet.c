b64605116fcf552afd5cce08a16a692fea35d4fd
get1221 <get1221smart@gmail.com>
2022-07-05 04:59:08 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ * Return: always 0.
+ */
+void print_alphabet(void)
+{
+	char getalpha = 'a';
+
+	while (getalpa <= 'z')
+	{
+		_putchar (getalpha);
+		getalpha++
+	}
+	_putchar('\n');
+}
