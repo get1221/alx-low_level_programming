0af27a144cf6a6c940014205d42bf997995459e2
get1221 <get1221smart@gmail.com>
2022-07-05 06:43:54 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - absolute value of a number
+ * @n: integer number solve absolute value 
+ * Return: sucess 
+ */
+int _abs(int);
+{
+	if (n < 0)
+	{
+		return (n * - 1);
+	}
+	return (0);
+}
