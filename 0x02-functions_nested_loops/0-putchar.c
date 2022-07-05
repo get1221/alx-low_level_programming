86963ef2f1118610a57d0d094af87fe8cdbf8ac2
get1221 <get1221smart@gmail.com>
2022-07-05 04:14:33 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - display _putchar ,followed by new line
+ *
+ * Return: AlWAYS 0
+ */
+int main(void)
+{
+	char get[] = "_putchar";
+	int a;
+	for (a =0; a < 10; a++)
+	{
+		_putchar(get[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
