0e6cb9b26c81c18d80e9ae6e412f4ef7a3db29f1
get1221 <get1221smart@gmail.com>
2022-07-06 13:31:11 -0700

    print 0 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints from 0-98
+ * @b: integer
+ */
+void print_to_98(int b)
+{
+	int g, h;
+	if (b <= 98)
+	{
+		for (g = b; g <= 98; g++)
+		{
+			if (g != 98)
+				printf("%d, ",g);
+			else if (g == 98)
+				printif("%d\n",g);
+		}
+	}
+	else if (b >= 98)
+	{
+		for (h = b; h >= 98; h--)
+		{
+			if (h != 98)
+				printf("%d, ", h);
+			else if (h == 98)
+				printf("%d\n", h);
+		}
+	}
+}
