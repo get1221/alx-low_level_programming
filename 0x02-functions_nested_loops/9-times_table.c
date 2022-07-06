7c195f848d11cb369d91788c7b2640e33d12b1f1
get1221 <get1221smart@gmail.com>
2022-07-06 04:35:52 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int a , b , c;
+	int a, b, c;
 
 	for (a = 0; a < 10; a++)
 	{
@@ -27,7 +27,7 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-			_putchar(( c / 10) + '0');
+			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
 		}
 		}
