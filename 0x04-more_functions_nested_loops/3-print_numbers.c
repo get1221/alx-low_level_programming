aa4ec27441238b853143161310de8a063cb70bf9
get1221 <get1221smart@gmail.com>
2022-07-07 07:27:45 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar("\n");
+	return (0)
+}
