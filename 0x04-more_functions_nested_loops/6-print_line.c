9be48c2ab139cfe75a573201d5cb32140e77da4b
get1221 <get1221smart@gmail.com>
2022-07-07 12:28:13 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - print line
+ * @n: the number of characteruse
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
