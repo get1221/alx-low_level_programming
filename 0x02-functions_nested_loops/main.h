2f1c5ba460fb20f95011dcf260930f89d598d851
get1221 <get1221smart@gmail.com>
2022-07-07 06:34:08 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 /*
- * File: main.h
- * Desc: header file declare for all fun
- */
-int _putchar (char a);
+ *
+ *  * File: main.h
+ *
+ *   *
+ *
+ *    * Desc: Header file containing declartions for all function
+ *
+ *     */
+int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,7 +18,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
+void time_teble(void);
 int add(int, int);
 void print_to_98(int n);
+void print_time_table(int n);
 #endif
