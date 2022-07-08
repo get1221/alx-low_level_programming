18ec8ebf32ce86c06fb07fa80b830ca29d1085fe
get1221 <get1221smart@gmail.com>
2022-07-07 17:30:23 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,14 @@
  */
 void print_line(int n)
 {
-	int i;
+	int x;
 
-	for (i = 0 i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar('_');
+		for (x = 0 x < n; x++)
+			_putchar(95);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
