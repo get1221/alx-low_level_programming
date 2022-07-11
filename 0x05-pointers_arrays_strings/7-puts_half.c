b41c40746decff0b86cc9c56b5c0d6070aec13a4
get1221 <get1221smart@gmail.com>
2022-07-11 09:47:21 -0700

    print 2nd half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half -print the 2nd half
+ *
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len, g, h;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	g = (len - 1) / 2;
+	for (h = g + 1; str[h] != '\0'; h++)
+			{
+			_putchar(str[h]);
+			}
+	_putchar('\n');
+}
