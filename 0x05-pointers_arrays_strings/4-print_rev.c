f252367295496f5c09c04b3cf90c40c8893baba8
get1221 <get1221smart@gmail.com>
2022-07-11 09:10:24 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int g = 0;
+	int len;
+
+	for (len = 0; s[len] != '\0';len++)
+	{
+	}
+	for (g = len - 1; g >= 0; g--)
+	{
+		_putchar(s[g]);
+	}
+	_putchar('\n');
+}
