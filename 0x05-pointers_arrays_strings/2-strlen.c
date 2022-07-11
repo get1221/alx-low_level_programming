ec055a7b16da6f8869d6e77bd5c58d635756c126
get1221 <get1221smart@gmail.com>
2022-07-11 10:03:16 -0700

    string lenght

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _strlen -len of string
+ *
  * @s: the length of string
  * Return: char s length
  */
@@ -9,6 +10,7 @@ int _strlen(char *s)
 	int g;
 
 	int len = 0;
+
 	for (g = 0; s[g] != '\0'; g++)
 	{
 		len++;
