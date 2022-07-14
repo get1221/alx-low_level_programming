fea592ab6b52b364f4abfa91a692b00333e29fce
get1221 <get1221smart@gmail.com>
2022-07-14 05:15:56 -0700

    change to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * string_toupper - function change to uppercase
+ * @n: change character
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int g;
+
+	g = 0;
+
+	while (n[g] != '\0')
+	{
+		if (n[g] >= 'a' && n[g] <= 'z')
+			n[g] = n[g] - 32;
+		g++
+	}
+	return (n);
+}
+
