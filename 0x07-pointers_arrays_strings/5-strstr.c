3c5dcc7c8c8917593d68d231e2f8cceb2d8bda91
get1221 <get1221smart@gmail.com>
2022-07-18 04:49:21 -0700

    locate the character

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,2 +1,33 @@
 #include "main.h"
+/**
+ * _strstr - function to locate
+ *
+ * @haystack: pointer to character
+ *
+ * @needle: pointer to character
+ *
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *res = haystack, *funneedle = needle
 
+		while (*haystack)
+		{
+			while (*needle)
+			{
+				if (*haystack++ != *needle++)
+				{
+					break;
+				}
+			}
+			if (!*needle)
+			{
+				return (res);
+			}
+			needle = funneedle;
+			res++;
+			haystack = res;
+		}
+	return (0);
+}
