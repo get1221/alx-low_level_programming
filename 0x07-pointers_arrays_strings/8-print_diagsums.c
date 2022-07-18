0fb49c6c61ab4ceed80bcdafe018d5bdd24485e8
get1221 <get1221smart@gmail.com>
2022-07-18 10:18:41 -0700

    print diagonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print diagonal sum
+ *
+ * @a: a pointer points
+ * @size: size
+ */
+void print_diagsums(int *a, int size)
+{
+	int g, sumg1 = 0, sumg2 = 0;
+
+	for (g = 0; g < size; g++)
+	{
+		sumg1 += *(a + (size * g + g));
+		sumg2 += *(a + (size * g + size - 1  - g));
+	}
+	printf("%d, ", sumgl);
+	printf("%d\n", sumg2);
+}
