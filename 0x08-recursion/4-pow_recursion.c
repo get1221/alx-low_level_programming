1eabbad944dd2414175d4b21b8e01e82bd792cac
get1221 <get1221smart@gmail.com>
2022-07-20 07:36:08 -0700

    power of x in recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - rec
+ * @x: integer x
+ * @y: integer 2
+ * Return: power of x
+ */
+int _pow_recursion(int x, in y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
