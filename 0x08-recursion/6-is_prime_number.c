f777584271cb5b231fbd42041ad40042febb66fe
get1221 <get1221smart@gmail.com>
2022-07-20 09:45:30 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+int tmp_prime(int n, int i);
+/**
+ * divisors - no prime
+ * @n: int
+ * @m: integer
+ * Return: prime
+ */
+
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - prime check
+ * @n: integer
+ * Return: prime
+ */
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2)) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
