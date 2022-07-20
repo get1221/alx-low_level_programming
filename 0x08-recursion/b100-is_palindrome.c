803de41da5b23807a87fdd570e8bd94e65b3bc8a
get1221 <get1221smart@gmail.com>
2022-07-20 10:03:07 -0700

    palindrome

---
+++ 0x08-recursion/b100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * * _strlen_recursion - size
+ * @s: pointer
+ * Return: rec
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * pl - palindrome
+ * @s: pointer
+ * @l: pos
+ * Return: pal
+ */
+int pl(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + 1))
+	{
+		return (pl(s + 1, 1 - 2));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - palindrome
+ * @s: pointer
+ * Return: Recu
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	return (p1(s, len - 1));
+}
