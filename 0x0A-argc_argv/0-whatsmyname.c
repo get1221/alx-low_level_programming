8730a489eebfe4ce465feaa2f42408eccfe33fd5
get1221 <get1221smart@gmail.com>
2022-07-22 09:30:38 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: number of args
+ * @argv: array that contain the program comd
+ * Return: 0 - success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
