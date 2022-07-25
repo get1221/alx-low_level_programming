12e221107c796d156a6577cf59321dfff34883d8
get1221 <get1221smart@gmail.com>
2022-07-25 02:00:55 -0700

    print a min coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,18 +20,29 @@ int main(int argc, char *argv[])
 
 	while (cents > 0)
 	{
-		if (cents >= 25)
+		Getcoins++;
+		if ((cents - 25) >= 0)
+		{
 			cents -= 25;
-		else if (cents >= 10)
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
 			cents -= 10;
-		else if (cents >= 5)
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
 			cents -= 5;
-		else if (cents >= 2)
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
 			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		Getcoins += 1;
+			continue;
+		}
+		cents--;
 	}
-	printf"(%d\n", Getcoins);
+	printf("%d\n", Getcoins);
 	return (0);
 }
