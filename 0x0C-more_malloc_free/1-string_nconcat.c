5dad81b5b6e7a62607101f1eb8253f54162bce2b
get1221 <get1221smart@gmail.com>
2022-07-28 07:15:06 -0700

    string concate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - calculate and return string len
+ *
+ * @string: string
+ *
+ * Return: string len
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcate - concate atring 1 and n bytes of string 2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n byte to concate
+ * Return: pointer to concate
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int num, len, i, j;
+
+	num = n;
+
+	if (s1 == NULL) /* ACC FOR NULL STR*/
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0) /* acc for negative byte*/
+		
+		if (num >= _strlen(s2)) /* string*/
+			len = _strlen(s1) + num + 1;
+
+	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL);
+	return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
+}
