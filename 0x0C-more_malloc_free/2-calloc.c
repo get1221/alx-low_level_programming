b57bcd27eef7e2bec6e95868d00b7f2cab3388d1
get1221 <get1221smart@gmail.com>
2022-07-28 07:45:32 -0700

    concate

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocate memory 
+ * @nmemb: number of elements
+ * @size: size of elements
+ * Return: NULL IS SIZE OF MEMB
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size)
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+	return (p);
+}
