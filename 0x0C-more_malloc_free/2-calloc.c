95f7d9da309da4ac4d226c2c3b491b8bae3d0332
get1221 <get1221smart@gmail.com>
2022-07-29 08:19:12 -0700

    function to perform calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,20 +8,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size)
 		if (p == NULL)
-		{
 			return (NULL);
-		}
-
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		*((char *)(p) + i) = 0;
-	}
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
 	return (p);
 }
