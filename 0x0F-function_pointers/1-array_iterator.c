5e7cdc352616b97fab94b6490af8510c61a1be38
get1221 <get1221smart@gmail.com>
2022-08-03 10:36:27 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - execute function
+ * @array: array
+ * @size: size
+ * @action: pointer to function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i<(int) size; i++)
+	{
+		actions(array[i]);
+	}
+}
