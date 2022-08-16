59ae23889371057b419a7e1c6a668b97550c17f4
get1221 <get1221smart@gmail.com>
2022-08-16 06:58:32 -0700

    free lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 #include <stdlib.h>
-/*
+/**
  * free_listint - free a list
  * @head: Address of the first node of a list
- */
+**/
 void free_listint(listint_t *head)
 {
 	listint_t *tp, *tp2;
