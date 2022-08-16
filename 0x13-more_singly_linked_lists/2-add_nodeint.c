721c8c74c17bcd1413e56ea09de911e67a5f987b
get1221 <get1221smart@gmail.com>
2022-08-16 06:38:01 -0700

    add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_nodeint - Add a new node ata the first
+ * @head: address of the first node
+ * @n: Integer to inssert into the new node 
+ * Return: Address of the new node 
+ */
+listint_t *add_nodeint(listint_t **head, conts int n)
+{
+	listint_t *tp;
+
+	tp = malloc(sizeof(listint_t));
+	if (tp == NULL)
+		return (NULL);
+	tp->n = n;
+	tp->next = *head;
+	*head = tp;
+	return (*head);
+}
