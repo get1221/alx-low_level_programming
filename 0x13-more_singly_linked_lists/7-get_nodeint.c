e24969d10555166a2b8464aa7a4b305615602514
get1221 <get1221smart@gmail.com>
2022-08-16 09:15:23 -0700

    get node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - find a node in a list
+ * @head: Address of the first node in a list
+ * @index: postion of the node to find
+ * Return: Node Address
+ **/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int y = 0;
+	if (head == NULL)
+		return (NULL);
+	for (y = 0; y < index; y++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
+}
